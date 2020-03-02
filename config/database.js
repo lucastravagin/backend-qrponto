570fdd22e65d59b38fc73eacdc0be86c4a9d4d85
oursys <admin@oursys.com.br>
2020-03-02 02:29:59 -0300

    Subindo versão final

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 mongoose.Promise = global.Promise;
-module.exports = mongoose.connect('mongodb://localhost/pokeapp', { useNewUrlParser: true, keepAlive: true, reconnectTries: Number.MAX_VALUE, useMongoClient: true })
+module.exports = mongoose.connect('mongodb://qrponto:ZByiUcSGr1alEm2m@cluster0-shard-00-00-dyqdr.mongodb.net:27017,cluster0-shard-00-01-dyqdr.mongodb.net:27017,cluster0-shard-00-02-dyqdr.mongodb.net:27017/qrponto?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority', { useNewUrlParser: true, keepAlive: true, reconnectTries: Number.MAX_VALUE, useMongoClient: true })
 
 mongoose.Error.messages.general.required = "O atributo '{PATH}' é obrigatório."
 mongoose.Error.messages.Number.min = "O '{VALUE}' informado é menor que o limite mínimo de '{MIN}'."
